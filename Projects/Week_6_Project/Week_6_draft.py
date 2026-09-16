5eec0ee343d4e3af5e1bdb7da217344ea3db52f5
Berna Barillo <btbarillo@gmail.com>
2026-09-16 23:40:56 +0800

    Commit week 6 proj draft

---
+++ Projects/Week_6_Project/Week_6_draft.py
@@ -0,0 +1,9 @@
+import json
+import logging
+import os
+from datetime import date, timedelta
+from pathlib import Path
+import pandas as pd
+import requests
+from sqlalchemy import create_engine, text
+
